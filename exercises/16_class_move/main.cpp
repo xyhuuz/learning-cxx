10beffaf9ca9386f359aec4a4ce0b1c70d5411ed
xyhuuz <tdcqxyhy@outlook.com>
2025-01-28 00:04:46 +0800

    3

--- exercises/16_class_move/main.cpp
+++ exercises/16_class_move/main.cpp
@@ -8,28 +8,28 @@
 // READ: 移动构造函数 <https://zh.cppreference.com/w/cpp/language/move_constructor>
 // READ: 移动赋值 <https://zh.cppreference.com/w/cpp/language/move_assignment>
 // READ: 运算符重载 <https://zh.cppreference.com/w/cpp/language/operators>
+#include <cassert>
 
 class DynFibonacci {
     size_t *cache;
     int cached;
 
 public:
-    // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(0) {
-		cache[0]=0;
-		cache[1]=1;
-	}
+    // 实现动态设置容量的构造器
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
-    // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci &&) noexcept: cache(other.cache), cached(other.cached){
-		other.cache = nullptr;
+    // 实现移动构造器
+    DynFibonacci(DynFibonacci &&other) noexcept: cache(other.cache), cached(other.cached) {
+        other.cache = nullptr;
         other.cached = 0;
-	}
+    }
 
-    // TODO: 实现移动赋值
-    // NOTICE: ⚠ 注意移动到自身问题 ⚠
-    DynFibonacci &operator=(DynFibonacci &&) noexcept {
-		if (this != &other) {
+    // 实现移动赋值
+    DynFibonacci &operator=(DynFibonacci &&other) noexcept {
+        if (this != &other) {
             delete[] cache;  // 释放原来的内存
             cache = other.cache;
             cached = other.cached;
@@ -37,16 +37,16 @@ public:
             other.cached = 0;
         }
         return *this;
-	}
+    }
 
-    // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci(){
-		delete[] cache;
-	}
+    // 实现析构器，释放缓存空间
+    ~DynFibonacci() {
+        delete[] cache;
+    }
 
-    // TODO: 实现正确的缓存优化斐波那契计算
+    // 实现正确的缓存优化斐波那契计算
     size_t operator[](int i) {
-		if (i <= cached) {
+        if (i < cached) {
             return cache[i];  // 如果已经计算过了，直接返回
         }
 
@@ -56,35 +56,34 @@ public:
         }
         cached = i + 1;  // 更新已缓存的数量
         return cache[i];
-        
     }
 
-    // NOTICE: 不要修改这个方法
+    // 不要修改这个方法
     size_t operator[](int i) const {
-        ASSERT(i <= cached, "i out of range");
+        assert(i < cached && "i out of range");
         return cache[i];
     }
 
-    // NOTICE: 不要修改这个方法
+    // 不要修改这个方法
     bool is_alive() const {
-        return cache;
+        return cache != nullptr;
     }
 };
 
 int main(int argc, char **argv) {
     DynFibonacci fib(12);
-    ASSERT(fib[10] == 55, "fibonacci(10) should be 55");
+    assert(fib[10] == 55 && "fibonacci(10) should be 55");
 
     DynFibonacci const fib_ = std::move(fib);
-    ASSERT(!fib.is_alive(), "Object moved");
-    ASSERT(fib_[10] == 55, "fibonacci(10) should be 55");
+    assert(!fib.is_alive() && "Object moved");
+    assert(fib_[10] == 55 && "fibonacci(10) should be 55");
 
     DynFibonacci fib0(6);
     DynFibonacci fib1(12);
 
     fib0 = std::move(fib1);
     fib0 = std::move(fib0);
-    ASSERT(fib0[10] == 55, "fibonacci(10) should be 55");
+    assert(fib0[10] == 55 && "fibonacci(10) should be 55");
 
     return 0;
-}
+}//12
