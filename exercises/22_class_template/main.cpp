92bcd1f55393d2f24792dfb3fc72535509840737
xyhuuz <tdcqxyhy@outlook.com>
2025-01-28 00:24:57 +0800

    5

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,4 +1,7 @@
 ﻿#include "../exercise.h"
+#include <cstring>
+
+// READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
 template<class T>
 struct Tensor4D {
@@ -7,10 +10,10 @@ struct Tensor4D {
 
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
-        for (int i = 0; i < 4; ++i) {
-            shape[i] = shape_[i];
-            size *= shape[i];
-        }
+        // TODO: 填入正确的 shape 并计算 size
+        std::memcpy(shape, shape_, 4 * sizeof(unsigned int));
+        size = shape[0] * shape[1] * shape[2] * shape[3];
+
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -71,20 +74,20 @@ int main(int argc, char **argv) {
         unsigned int shape[]{1, 2, 3, 4};
         // clang-format off
         int data[]{
-             1,  2,  3,  4,
-             5,  6,  7,  8,
-             9, 10, 11, 12,
+            1,  2,  3,  4,
+            5,  6,  7,  8,
+            9, 10, 11, 12,
 
             13, 14, 15, 16,
             17, 18, 19, 20,
             21, 22, 23, 24};
-        // clang-format on
-        auto t0 = Tensor4D(shape, data);
-        auto t1 = Tensor4D(shape, data);
-        t0 += t1;
-        for (auto i = 0u; i < sizeof(data) / sizeof(*data); ++i) {
-            ASSERT(t0.data[i] == data[i] * 2, "Tensor doubled by plus its self.");
-        }
+            // clang-format on
+            auto t0 = Tensor4D(shape, data);
+            auto t1 = Tensor4D(shape, data);
+            t0 += t1;
+            for (auto i = 0u; i < sizeof(data) / sizeof(*data); ++i) {
+                ASSERT(t0.data[i] == data[i] * 2, "Tensor doubled by plus its self.");
+            }
     }
     {
         unsigned int s0[]{1, 2, 3, 4};
@@ -97,46 +100,46 @@ int main(int argc, char **argv) {
             4, 4, 4, 4,
             5, 5, 5, 5,
             6, 6, 6, 6};
-        // clang-format on
-        unsigned int s1[]{1, 2, 3, 1};
-        // clang-format off
-        float d1[]{
-            6,
-            5,
-            4,
+            // clang-format on
+            unsigned int s1[]{1, 2, 3, 1};
+            // clang-format off
+            float d1[]{
+                6,
+                5,
+                4,
 
-            3,
-            2,
-            1};
-        // clang-format on
+                3,
+                2,
+                1};
+                // clang-format on
 
-        auto t0 = Tensor4D(s0, d0);
-        auto t1 = Tensor4D(s1, d1);
-        t0 += t1;
-        for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
-            ASSERT(t0.data[i] == 7.f, "Every element of t0 should be 7 after adding t1 to it.");
-        }
+                auto t0 = Tensor4D(s0, d0);
+                auto t1 = Tensor4D(s1, d1);
+                t0 += t1;
+                for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
+                    ASSERT(t0.data[i] == 7.f, "Every element of t0 should be 7 after adding t1 to it.");
+                }
     }
     {
         unsigned int s0[]{1, 2, 3, 4};
         // clang-format off
         double d0[]{
-             1,  2,  3,  4,
-             5,  6,  7,  8,
-             9, 10, 11, 12,
+            1,  2,  3,  4,
+            5,  6,  7,  8,
+            9, 10, 11, 12,
 
             13, 14, 15, 16,
             17, 18, 19, 20,
             21, 22, 23, 24};
-        // clang-format on
-        unsigned int s1[]{1, 1, 1, 1};
-        double d1[]{1};
+            // clang-format on
+            unsigned int s1[]{1, 1, 1, 1};
+            double d1[]{1};
 
-        auto t0 = Tensor4D(s0, d0);
-        auto t1 = Tensor4D(s1, d1);
-        t0 += t1;
-        for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
-            ASSERT(t0.data[i] == d0[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
-        }
+            auto t0 = Tensor4D(s0, d0);
+            auto t1 = Tensor4D(s1, d1);
+            t0 += t1;
+            for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
+                ASSERT(t0.data[i] == d0[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
+            }
     }
 }
